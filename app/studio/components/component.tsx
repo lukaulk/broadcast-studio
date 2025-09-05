698d8114f5193e26f7c8fd31cefa71fc130ec4e7
lukaulk <lukaundombele@gmail.com>
2025-09-05 17:36:36 +0200

    Add router diagram image to public directory

--- app/studio/components/component.tsx
+++ app/studio/components/component.tsx
@@ -8,14 +8,13 @@ interface ComponentProps {
 }
 export default function Component( { name, type, icon, description }: ComponentProps ) {
   return (
-    <div className="flex space-x-4 rounded-sm overflow-x-auto py-2 px-4 h-full items-center hover:scale-105 transition-all cursor-grab active:opacity-90 active:border active:border-blue-500" title={description}>
-      <div className="flex flex-col  items-center w-30 h-30 justify-center">
+  
+      <div className="flex flex-col items-center w-30 h-30 justify-center hover:scale-105 py-2 px-4 transition-all rounded-sm cursor-grab active:opacity-90 active:border active:border-blue-500" title={description}>
         {icon !== undefined  && (
             <Image alt="TV" src={icon} width={100} height={100} />
         )}
         <span className="text-sm font-semibold mt-1">{name}</span>
         <span className="text-[12px] opacity-75 font-semibold">{type}</span>
       </div>
-    </div>
   );
 }
--- app/studio/components/elementBar.tsx
+++ app/studio/components/elementBar.tsx
@@ -5,31 +5,33 @@ export default function ElementBar() {
     <div className="flex w-full justify-center">
       <div className="flex min-h-38 max-h-40 rounded-none border-t border-t-[var(--bsui-border)] w-full items-center bg-[var(--bsui-gray-3)] text-[var(--bsui-gray-0)] shadow-md">
         {/* Components Horizontal Scroll List */}
-        
-          <div className="flex items-center min-w-20">
-           <Component icon="/dvc/tv.png" name="TV" type="End Device"/>
-           <Component icon="/dvc/tv.png" name="TV" type="End Device"/>
-           <Component icon="/dvc/tv.png" name="TV" type="End Device"/>
-    
 
+        <div className="flex items-center min-w-20">
+          <div className="flex space-x-4 items-center">
+            <Component icon="/dvc/tv.png" name="TV" type="End Device" />
+            <Component icon="/dvc/pc.png" name="TV" type="End Device" />
+            <Component icon="/dvc/Monitor.png" name="TV" type="End Device" />
+            <Component icon="/dvc/router.png" name="TV" type="Wireless Device" />
+            <Component icon="/dvc/Switch.png" name="TV" type="Connect Device" />
+            <Component icon="/dvc/Desktop.png" name="TV" type="End Device" />
             <div className="w-12 h-12 bg-[var(--bsui-gray-2)] border border-[var(--bsui-border)] rounded-md flex items-center justify-center hover:bg-[var(--bsui-active)] active:scale-95 cursor-pointer">
-              <svg
-                aria-hidden="true"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth={1.5}
-                stroke="currentColor"
-                className="size-6 text-white"
-              >
-                <path strokeLinecap="round" strokeLinejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
-              </svg>
-            </div>
-
-
+            <svg
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="size-6 text-white"
+            >
+              <path strokeLinecap="round" strokeLinejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
+            </svg>
           </div>
+          </div>
+
           
         </div>
       </div>
+    </div>
   );
 }
