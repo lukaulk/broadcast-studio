b7ffdc5a6ff2854ad35c1296a28e4924d640e60e
lukaulk <lukaundombele@gmail.com>
2025-09-17 21:11:57 +0200

    fix: correct type in Component instances and enhance styling in ElementBar

--- app/studio/components/component.tsx
+++ app/studio/components/component.tsx
@@ -10,7 +10,7 @@ interface ComponentProps {
 export default function Component({ name, type, icon, description }: ComponentProps) {
   return (
     <div
-      className="group relative flex overflow-hidden items-center justify-center w-40 h-40 hover:scale-105 transition-transform rounded-md overflow-hidden cursor-grab active:opacity-90 active:border active:border-blue-500"
+      className="group relative flex items-center justify-center w-40 h-40 hover:scale-105 transition-transform rounded-md overflow-hidden cursor-grab active:opacity-90 active:border active:border-blue-500"
       title={description}
     >
       {icon && (
@@ -18,7 +18,7 @@ export default function Component({ name, type, icon, description }: ComponentPr
           alt={name ?? "icon"}
           src={icon}
           fill
-          className="object-fit scale-80 transition-all duration-500 ease-in-out group-hover:scale-110"
+          className="object-contain scale-90 transition-all duration-500 ease-in-out group-hover:scale-110 drop-shadow-black/10 drop-shadow-2xl"
         />
       )}
       <div className="absolute inset-0 flex flex-col items-center justify-center bg-black/50 backdrop-blur-sm opacity-0 transition-opacity duration-300 ease-in-out group-hover:opacity-100">
--- app/studio/components/elementBar.tsx
+++ app/studio/components/elementBar.tsx
@@ -2,14 +2,15 @@ import Component from "./component";
 
 export default function ElementBar() {
   return (
-    <div className="flex w-full justify-center">
+    <div className="flex w-full justify-center select-none">
       <div className="flex min-h-40 max-h-45 rounded-none border-t border-t-[var(--bsui-border)] w-full items-center bg-[var(--bsui-gray-3)] text-[var(--bsui-gray-0)] shadow-md">
         <div className="flex items-center min-w-20">
           <div className="flex space-x-4 items-center">
-            <Component icon="/dvc/laptop.png" name="PC" type="Endq Device" />
+            <Component icon="/dvc/laptop.png" name="PC" type="End Device" />
             <Component icon="/dvc/router.png" name="Router" type="Wireless Device" />
             <Component icon="/dvc/switch.png" name="Switch" type="Connect Device" />
-            <Component icon="/dvc/server.png" name="Switch" type="End Device" />
+            <Component icon="/dvc/server.png" name="Server" type="End Device" />
+            <Component icon="/dvc/smartphone.png" name="Smartphone" type="End Device" />
             <Component icon="/dvc/desktop.png" name="Desktop" type="End Device" />
             
             <div className="w-12 h-12 bg-[var(--bsui-gray-2)] border border-[var(--bsui-border)] rounded-md flex items-center justify-center hover:bg-[var(--bsui-active)] active:scale-95 cursor-pointer">
